8743f887ee2ff96afd605a9cfb0dfe6dffa0c9a0
Jeff Whittington <jeff@codecology.com>
2021-03-08 20:13:29 -0800

    Setup build directory to serve as subdirectory

---
+++ build/precache-manifest.2041c2b159e2da766f78904ef6fddaa6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60e1b58a52a4f2220ec603514e286fc0",
+    "url": "/ca-covid/index.html"
+  },
+  {
+    "revision": "2dcf646cf9ed59273dcb",
+    "url": "/ca-covid/static/css/main.a4638dee.chunk.css"
+  },
+  {
+    "revision": "6ed920acfaadc7c4e6bd",
+    "url": "/ca-covid/static/js/2.65df0145.chunk.js"
+  },
+  {
+    "revision": "2dcf646cf9ed59273dcb",
+    "url": "/ca-covid/static/js/main.be75543c.chunk.js"
+  },
+  {
+    "revision": "160d824c5d88c15a3107",
+    "url": "/ca-covid/static/js/runtime~main.c8b77a6b.js"
+  }
+]);
